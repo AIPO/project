47297d919677a53cab17c15718f3c93f7cc1d05d
povilas <p.vaitonis@gmail.com>
2017-10-23 15:34:43 +0300

    store and update methods created : needs refactoring

--- app/Comment.php
+++ app/Comment.php
@@ -8,6 +8,8 @@ class Comment extends Model
 {
     protected $fillable = ['comment', 'votes', 'spam', 'reply_id', 'page_id', 'users_id'];
     protected $dates = ['created_at', 'updated_at'];
+
+
     public function replies()
     {
         return $this->hasMany('App\Comment','id','reply_id');
--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Comment;
+use App\CommentVote;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -30,18 +31,27 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request $request
+     * @return array
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'comment' => 'required',
+            'reply_id' => 'filled',
+            'page_id' => 'filled',
+            'users_id' => 'required',
+        ]);
+        $comment = Comment::create($request->all());
+        if ($comment) {
+            return ["status" => "true", "commentId" => $comment->id];
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Comment  $comment
+     * @param  \App\Comment $comment
      * @return \Illuminate\Http\Response
      */
     public function show(Comment $comment)
@@ -52,7 +62,7 @@ class CommentController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Comment  $comment
+     * @param  \App\Comment $comment
      * @return \Illuminate\Http\Response
      */
     public function edit(Comment $comment)
@@ -63,19 +73,73 @@ class CommentController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Comment  $comment
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Comment $comment
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Comment $comment)
+    public function update(Request $request, $commentId, $type)
     {
-        //
+        if ($type == "vote") {
+            $this->validate($request, [
+                'vote' => 'required',
+                'users_id' => 'required'
+            ]);
+            $comments = Comment::find($commentId);
+            $data = [
+                'comment_id' => $commentId,
+                'vote' => $request->vote,
+                'user_id' => 'users_id',
+            ];
+            if ($request->vote == "up") {
+                $comment = $comments->first();
+                $vote = $comment->vote;
+                $vote++;
+                $comment->vote = $vote;
+                $comments->save();
+            }
+            if ($request->vote == "down") {
+                $comment = $comments->first();
+                $vote = $comment->vote;
+                $vote--;
+                $comment->vote = $vote;
+                $comments->save();
+            }
+            if(CommentVote::create($data))
+                return "true";
+        }
+        if($type == "spam"){
+            $this->validate($request,[
+                "users_id" => 'required'
+            ]);
+            $comments = Comment::find($commentId);
+
+            $comment = $comments->first();
+
+            $spam = $comment->spam;
+
+            $spam++;
+
+            $comments->spam = $spam;
+
+            $comments->save();
+
+            $data = [
+
+                "comment_id" => $commentId,
+
+                'user_id' => $request->users_id,
+
+            ];
+            if(CommentSpam::create($data))
+
+                return "true";
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Comment  $comment
+     * @param  \App\Comment $comment
      * @return \Illuminate\Http\Response
      */
     public function destroy(Comment $comment)
